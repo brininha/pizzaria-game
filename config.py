09b37d940e1c8bf173866f4f1fc1811cd3f84090
Sabrina Cristan <sabrina.cristan@gmail.com>
2026-09-10 19:35:27 -0300

    feat: adiciona arquivo config.py e define as constantes padroes

---
+++ config.py
@@ -0,0 +1,8 @@
+# endereco de loopback, eh o localhost, nao passa por uma rede externa, mas nos ajuda nos testes locais
+HOST = '127.0.0.1'
+# porta escolhida, normalmente nao fica ocupada
+PORT = 5050
+# espaco reservado na memoria para guardar temporariamente dados que chegam da rede
+BUFFER_SIZE = 1024
+# padrao de conversao
+ENCODING = 'utf-8'
