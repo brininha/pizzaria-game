539d96557dacdd644dc858ee0169089529abeceb
Sabrina Cristan <sabrina.cristan@gmail.com>
2026-09-13 23:02:04 -0300

    feat(servidor): implementa logica de recebimento de dados

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,13 +1,14 @@
 import socket
 from config import HOST, PORT
+from protocolo import *
 
 def iniciar_servidor():
-    #Instancia o socket utilizando IPv4 e TCP
+    # Instancia o socket utilizando IPv4 e TCP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-    #Associa o socket ao IP e porta e o coloca em modo de escuta
+    # Associa o socket ao IP e porta e o coloca em modo de escuta
     server_socket.bind((HOST, PORT))
     server_socket.listen()
     
@@ -19,10 +20,20 @@ if __name__ == "__main__":
     server_socket = iniciar_servidor()
 
     try:
-         # Issue 3: aceita UMA conexão por enquanto (bloqueante)
+        # Issue 3: aceita UMA conexão por enquanto (bloqueante)
         # A partir da Issue 7 isso vira um loop com threading
         conexao, endereco = server_socket.accept()
         ip_cliente, porta_cliente = endereco
         print(f"[SERVIDOR] Cliente conectado: {ip_cliente}:{porta_cliente}")
+        
+        dados = conexao.recv(1024)
+        texto_decodificado = dados.decode('utf-8')
+        
+        comando, payload = interpretar_mensagem(texto_decodificado)
+        
+        if comando == 'ECHO':
+            resposta_bytes = formatar_mensagem("ECHO_REPLY", payload)
+            conexao.sendall(resposta_bytes)
+
     finally:
         server_socket.close()
