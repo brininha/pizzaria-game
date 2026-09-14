c879c0a0840b2cc6026d254dde70c6b01fc2c757
Sabrina Cristan <sabrina.cristan@gmail.com>
2026-09-13 23:01:32 -0300

    feat(cliente): implementa envio e recebimento de mensagens no cliente

--- cliente/main.py
+++ cliente/main.py
@@ -1,10 +1,17 @@
 from rede import conectar_servidor
+from protocolo import *
 
 def main():
     client_socket = conectar_servidor()
-
+    
     try:
-        pass  # próximas issues: enviar AUTH_CONN, escutar servidor, etc.
+        mensagem_bytes = formatar_mensagem("ECHO", "Teste de conexao")
+        client_socket.sendall(mensagem_bytes)
+        
+        dados_recebidos = client_socket.recv(1024)
+        texto_decodificado = dados_recebidos.decode('utf-8')
+        comando, payload = interpretar_mensagem(texto_decodificado)
+        print(f"[RESPOSTA RECEBIDA]: {comando} - {payload}")
     finally:
         client_socket.close()
         print("[CLIENTE] Conexão encerrada.")
