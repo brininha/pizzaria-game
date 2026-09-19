15c7e0cc3f1a87b6d274bb21ca971a2bb830cb7d
Sabrina Cristan <sabrina.cristan@gmail.com>
2026-09-19 16:42:57 -0300

    feat(cliente): cria funcao para escutar o servidor

--- cliente/main.py
+++ cliente/main.py
@@ -1,6 +1,29 @@
 from cliente.rede import conectar_servidor
 from utils.protocolo import *
 
+# funcao isolada para recepcao de dados
+def escutar_servidor(client_socket):
+    try:
+        while True:
+            # trava a execução aguardando pacotes do servidor
+            dados = client_socket.recv(1024)
+            
+            # se dados vier vazio, o servidor encerrou a conexao
+            if not dados:
+                print("\n[CLIENTE] Conexão com o servidor encerrada.")
+                break
+                
+            # decodifica e separa o comando do payload
+            texto_decodificado = dados.decode('utf-8')
+            comando, payload = interpretar_mensagem(texto_decodificado)
+            
+            print(f"\n[MENSAGEM RECEBIDA]: {comando} {payload}")
+            
+    except ConnectionResetError:
+        print("\n[ERRO] O servidor foi desconectado abruptamente.")
+    except Exception as e:
+        print(f"\n[ERRO] Falha na recepção: {e}")
+
 def main():
     client_socket = conectar_servidor()
     
